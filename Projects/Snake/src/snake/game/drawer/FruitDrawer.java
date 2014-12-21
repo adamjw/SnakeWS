330fc97473e3bbd01f7bfa5c93e309350d1665f1
tamf <tam.fabian@gmail.com>
2014-12-20 21:08:57 -0800

    Revert "Revert "Issue 3""
    This reverts commit 2ec046da85f650bb65b7d354a838401cd06afa39.

--- Projects/Snake/src/snake/game/drawer/FruitDrawer.java
+++ Projects/Snake/src/snake/game/drawer/FruitDrawer.java
@@ -1,18 +1,19 @@
 package snake.game.drawer;
 
+import simpleio.common.Colour;
 import snake.game.state.Fruit;
 
 public class FruitDrawer {
 
 	private BoardDrawer boardDrawer;
-	
-	public FruitDrawer(BoardDrawer boardDrawer){
+
+	public FruitDrawer(BoardDrawer boardDrawer) {
 		this.boardDrawer = boardDrawer;
 	}
-	
-	
-	public void drawFruit(Fruit fruit){
+
+	public void drawFruit(Fruit fruit) {
+		boardDrawer.drawSquare(Colour.RED, fruit.getPosition());
 
 	}
-	
+
 }
